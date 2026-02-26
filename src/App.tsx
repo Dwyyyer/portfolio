470d3301807138f9fe13c023c8953b0544e1b175
Daniel <Dan.tib.araujo@gmail.com>
2026-02-26 02:19:35 -0300

    Feat: Vercel Analytics for counting visitors.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Analytics } from "@vercel/analytics/react"
 import Home from "./pages/Home/Home"
 
 const App = () => {
   return (
     <>
       <Home/>
+      <Analytics />
     </>
   )
 }
